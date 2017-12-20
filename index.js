fc854f84838f7ad6094eb2308bea578510c6a5b3
MinhPQ <minh.pq@ifs.com.vn>
2017-12-20 12:08:13 +0700

    Test deploy

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ io.on('connection', function(socket){
   console.log('a user connected');
   socket.on('disconnect', function(){
     console.log('user disconnected');
+    console.log('test');
+
   });
 });
 
